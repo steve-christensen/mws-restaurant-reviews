21d2ea81b178a46d1a26fc72cc77cc4d31dd3104
Steve Christensen <steve.christensen@protonmail.com>
2018-08-06 11:30:44 -0500

    chore: add browser sync and update gulpfile to launch browser and watch for changes

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,9 @@ const autoprefixer = require('gulp-autoprefixer');
 const rename = require('gulp-rename');
 const responsive = require('gulp-responsive');
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync');
 
+const server = browserSync.create();
 
 const srcRoot = './src';
 const buildRoot = './build';
@@ -122,5 +124,42 @@ const responsiveImages = () =>
 
 gulp.task('images', gulp.series(cleanImages, copyFixedImages, responsiveImages));
 
-//gulp.task('default', gulp.series(['clean',gulp.parallel(['images','copyFiles','buildJS','buildCSS','buildHTML'])]));
-gulp.task('default', gulp.series(['clean',gulp.parallel(['images','buildJS','buildCSS','buildHTML'])]));
+gulp.task('buildAll', gulp.series(['clean',gulp.parallel(['images','buildJS','buildCSS','buildHTML'])]));
+
+
+const reload = (done) => {
+  server.reload();
+  done();
+}
+
+const serve = (done) => {
+  server.init({
+    server: {
+      baseDir: buildRoot
+    }
+  });
+  done();
+}
+
+// watch HTML changes
+gulp.task('watchHTML', () => {
+  return gulp.watch(paths.html.src, gulp.series('buildHTML',reload));
+});
+
+// watch CSS changes
+gulp.task('watchCSS', () => {
+  return gulp.watch(paths.styles.src, gulp.series('buildCSS',reload));
+});
+
+// watch JS changes
+gulp.task('watchJS', () => {
+  return gulp.watch(`${srcRoot}/**/*.js`, gulp.series('buildJS',reload));
+});
+
+// watch all source Changes
+gulp.task('watchAll', gulp.parallel('watchHTML', 'watchJS', 'watchCSS'), (done) => {
+  done();
+});
+
+// BuildAll, launch app, and watch for changes
+gulp.task('default', gulp.series(['buildAll',serve,'watchAll']));
